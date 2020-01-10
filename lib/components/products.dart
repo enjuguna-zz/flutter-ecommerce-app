e26bda0cc8b56339065819711723718c0adaefbb
Eric Njuguna <eric7njuguna@gmail.com>
2020-01-10 09:36:41 +0300

    Added Recent Products, Changed overall look

--- lib/components/products.dart
+++ lib/components/products.dart
@@ -8,15 +8,109 @@ class Products extends StatefulWidget {
 class _ProductsState extends State<Products> {
   var product_list = [
     {
-      "name": "Blazer",
-      "Picture":"images/products/clothes",
-      "Price":"Blazer",
-      "Old Price":"Blazer",
-
-    }
+      "name": "Cherry-Van",
+      "picture": "images/Products/cherry-vanilla.jpg",
+      "price": 300,
+      "old_price": 330,
+    },
+    {
+      "name": "S'mores",
+      "picture": "images/Products/smores.jpg",
+      "price": 300,
+      "old_price": 330,
+    },
+    {
+      "name": "Tilia",
+      "picture": "images/Products/tilia.jpg",
+      "price": 2000,
+      "old_price": 2300,
+    },
+    {
+      "name": "Tusker",
+      "picture": "images/Products/tusker.jpg",
+      "price": 200,
+      "old_price": 250,
+    },
+    {
+      "name": "Summit",
+      "picture": "images/Products/summit.jpg",
+      "price": 200,
+      "old_price": 250,
+    },
+    {
+      "name": "Hennesy",
+      "picture": "images/Products/hen.png",
+      "price": 400,
+      "old_price": 450,
+    },
   ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.builder(
+        itemCount: product_list.length,
+        gridDelegate:
+            new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return Single_prod(
+            product_name: product_list[index]['name'],
+            prod_picture: product_list[index]['picture'],
+            prod_old_price: product_list[index]['old_price'],
+            prod_price: product_list[index]['price'],
+          );
+        });
+  }
+}
+
+class Single_prod extends StatelessWidget {
+  final product_name;
+  final prod_picture;
+  final prod_old_price;
+  final prod_price;
+
+  Single_prod({
+    this.product_name,
+    this.prod_picture,
+    this.prod_old_price,
+    this.prod_price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Hero(
+          tag: product_name,
+          child: Material(
+            child: InkWell(
+              onTap: () {},
+              child: GridTile(
+                  footer: Container(
+                    color: Colors.white70,
+                    child: ListTile(
+                      leading: Text(
+                        product_name,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      title: Text(
+                        "\$$prod_price",
+                        style: TextStyle(
+                            color: Colors.red, fontWeight: FontWeight.w800),
+                      ),
+                      subtitle: Text(
+                        "\$$prod_old_price",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w800,
+                            decoration: TextDecoration.lineThrough),
+                      ),
+                    ),
+                  ),
+                  child: Image.asset(
+                    prod_picture,
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          )),
+    );
   }
 }
